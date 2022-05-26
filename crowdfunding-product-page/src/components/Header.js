0ad38bfe9a7d8d23dac5ed6d3bc5fec978133f15
Renz Palo <renzpalo@gmail.com>
2022-05-26 13:30:27 +0800

    updates

--- crowdfunding-product-page/src/components/Header.js
+++ crowdfunding-product-page/src/components/Header.js
@@ -1,6 +1,20 @@
+import React, { useRef } from 'react';
+
 import Container from "./Grid/Container";
 
 const Header = () => {
+  const refNavMenu = useRef();
+
+  const handleHamburgerMenu = (e) => {
+    console.log(refNavMenu.current.style.background);
+
+    if (refNavMenu.current.style.display !== 'none') {
+      refNavMenu.current.style.display = 'block';
+    } else {
+      refNavMenu.current.style.display = 'none';
+    }
+  };
+
   return (
     <header>
       <Container>
@@ -8,10 +22,14 @@ const Header = () => {
           <a href="#" className="nav-brand">
             <img src="images/logo.svg" alt="crowdfund Logo" />
           </a>
-          <div className="nav-menu-hamburger">
-            <img src="images/icon-hamburger.svg" alt="Hamburger Icon" />
+          <div 
+            className="nav-menu-hamburger" 
+            onClick={handleHamburgerMenu}>
           </div>
-          <div className="nav-menu">
+          <div 
+            className="nav-menu" 
+            ref={refNavMenu}
+            >
             <ul className="nav-menu__links">
               <li><a href="/">About</a></li>
               <li><a href="/">Discover</a></li>
